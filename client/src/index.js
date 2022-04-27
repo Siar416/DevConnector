dfec56ff49a5455d40d10346013d6f642021af42
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-27 13:16:36 -0400

    created login and register component along with setting up react router

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
+// commented out strict mode for Link to work
+
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
